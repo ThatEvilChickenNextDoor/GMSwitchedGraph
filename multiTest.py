94b3e843408378d1e7d52fcce40824ef7292f125
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2019-07-17 10:45:58 -0400

    Update multiTest.py

--- multiTest.py
+++ multiTest.py
@@ -4,8 +4,9 @@ from multiprocessing import Pool
 import numpy as np
 import networkx as nx
 
-def calculate(g6):
-	pass
+def calculate(G,vecs):
+	print(id(vecs))
+	return G
 	
 if __name__ == "__main__":
 	tic=time.time() #record time at beginning
@@ -15,8 +16,13 @@ if __name__ == "__main__":
 	filepath=sys.argv[1] #save input filepath
 	with open(filepath, 'rb') as fp: #open input file
 		toTest=nx.read_graph6(fp.name)
+		
+	iterable=list("{0:b}".format(i).zfill(19) for i in range(2**19))
+	vecs=np.asarray([np.fromiter(i, int) for i in iterable])
+	pool=Pool(processes=10) #start mp pool
+	toPrint = pool.starmap(calculate, [(i,vecs) for i in toTest])
 	with open('output.g6', 'wb') as op: #create output file
-		for G in toTest:
+		for G in toPrint:
 			op.write(nx.to_graph6_bytes(G, header=False))
 	toc=time.time()-tic #calculate running time
 	print(toc)
