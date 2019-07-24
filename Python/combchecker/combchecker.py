b42396588b9c9f2988e6c29dd08aabd1875006e5
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2019-07-24 12:33:47 -0400

    Modified combchecker to test third line

--- Python/combchecker/combchecker.py
+++ Python/combchecker/combchecker.py
@@ -2,10 +2,14 @@ from itertools import combinations
 import numpy as np
 import networkx as nx
 print('imports complete')
+regen=np.load('regenerate.npy')
 seeds = np.asarray((
 ((1,)*4+(0,)*8)*3,
 ((0,)*4+(1,)*4+(0,)*4)*3,
-((0,)*8+(1,)*4)*3
+((0,)*8+(1,)*4)*3#,
+#(0,)*8+(1,)*4+(0,)*4+(1,)*4+(0,)*4+(1,)*4+(0,)*8,
+#(0,)*4+(1,)*4+(0,)*4+(1,)*4+(0,)*16+(1,)*4,
+#(1,)*4+(0,)*16+(1,)*4+(0,)*4+(1,)*4+(0,)*4
 ), 'uint8')
 print('seeds generated')
 combs = list(combinations(range(12),4))
@@ -27,8 +31,14 @@ del(first)
 del(second)
 del(third)
 print('cleaned up')
-comp = np.dot(vecs, seeds.T)
-print('comparison generated')
-mask = np.all(np.isin(comp, (4)), axis=1)
-print('mask generated')
-np.save('output', vecs[mask])
+
+for i in range(len(regen)):
+	s=np.block([[seeds],[regen[i]]])
+	comp = np.dot(vecs, s.T)
+	print('comparison generated')
+	mask = np.all(np.isin(comp, (4)), axis=1)
+	print('mask generated')
+	#np.save('output', vecs[mask])
+	print(len(vecs[mask]), i)
+
+print('done')
