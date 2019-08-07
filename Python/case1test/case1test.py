a767854abe1a79f5299521d213d6c55609fcedc3
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2019-08-06 22:14:01 -0400

    Update case1test.py

--- Python/case1test/case1test.py
+++ Python/case1test/case1test.py
@@ -44,36 +44,16 @@ def constructGraph():
 			if abs(res-EIGEN) < EPS:
 				verts[cache_size] = v
 				cache_size += 1
-	return verts
-
-@njit(cache=True)
-def combgen(comb):
-	top=np.array((1,0,0))
-	out=np.empty((11*495**3,50), np.uint8)
-	comb_vecs=np.zeros((495,12), np.uint8)
-	header=np.identity(11, np.uint8)
-	for idx, nums in enumerate(comb):
-		for n in nums:
-			comb_vecs[idx, n]=1
-	for head in range(11):
-		for first in range(495):
-			for second in range(495):
-				for third in range(495):
-					out[third + second*495 + first*495**2 + head*495**3]=np.concatenate((top,header[head],comb_vecs[first],comb_vecs[second],comb_vecs[third]))
-			if first % 50 == 0:
-				print(head, round(first*100/495,3))
-	print('done')
-	return out
-
-comb=tuple(combinations(range(12),4))
-vecs=combgen(comb)
+	return verts[:cache_size]
 
 filepath=sys.argv[1] #get input filepath
+vecspath=sys.argv[2]
 with open(filepath, 'rb') as fp: #open input file
 	s=fp.readline()
 	mat_inv[:]=stringtomat(s.strip())
+	vecs=np.load(vecspath)
 	verts=constructGraph()
 	np.save('verts', verts)
-print('Successfuly processed: {} vectors.'.format(len(verts))
+print('Successfuly processed: {} vectors.'.format(len(verts)))
 toc=time.time()-tic
 print('Time elapsed: {} seconds'.format(round(toc, 3)))
