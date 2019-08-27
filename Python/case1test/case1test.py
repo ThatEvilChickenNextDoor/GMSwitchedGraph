b4af75f318e5d9eb04ef3e01c5f74a8e898a3881
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2019-08-27 15:41:45 -0400

    Update case1test.py

--- Python/case1test/case1test.py
+++ Python/case1test/case1test.py
@@ -13,16 +13,16 @@ EPS= 0.000001
 
 mat_inv=np.empty((N,N), np.float32)
 
-vec_j=np.ones(N)
+vec_j=np.ones(N, np.float32)
 
-#seeds = np.asarray((
-#(1,0,1)+(0,)*11+((1,)*4+(0,)*8)*3,
-#(1,0,1)+(0,)*11+((0,)*4+(1,)*4+(0,)*4)*3,
-#(1,0,1)+(0,)*11+((0,)*8+(1,)*4)*3#,
+seeds = np.array((
+(1,0,1)+(0,)*11+((1,)*4+(0,)*8)*3,
+(1,0,1)+(0,)*11+((0,)*4+(1,)*4+(0,)*4)*3,
+(1,0,1)+(0,)*11+((0,)*8+(1,)*4)*3#,
 #(0,)*8+(1,)*4+(0,)*4+(1,)*4+(0,)*4+(1,)*4+(0,)*8,
 #(0,)*4+(1,)*4+(0,)*4+(1,)*4+(0,)*16+(1,)*4,
 #(1,)*4+(0,)*16+(1,)*4+(0,)*4+(1,)*4+(0,)*4
-#), np.uint8)
+), np.float32)
 
 def stringtomat(s):
 	I=np.identity(N)
@@ -30,20 +30,25 @@ def stringtomat(s):
 	mat=EIGEN*I-A
 	return np.linalg.inv(mat)
 
-@njit(cache=True)
+@njit
 def constructGraph():
 	cache_size=0
-	verts=np.empty_like(vecs)
-	for i, v in enumerate(vecs):
-		if i % 10000 == 0:
+	verts=np.asarray(np.empty_like(vecs))
+	for i in range(len(vecs)):
+		if i % 10000000 == 0:
 			print(i, '/', len(vecs), '\t', round(i*100/len(vecs),10), '%', '\t', cache_size, 'hits')
+		v=np.asarray(vecs[i], np.float32)
 		prod=np.dot(mat_inv, v)
-		res=np.dot(prod, vec_j)
-		if abs(res+1) < EPS:
-			res=np.dot(prod, v)
-			if abs(res-EIGEN) < EPS:
-				verts[cache_size] = v
-				cache_size += 1
+		if abs(np.dot(prod, vec_j)+1) < EPS:
+			if abs(np.dot(prod, v)-EIGEN) < EPS:
+				res = np.dot(prod, seeds[0])
+				if (abs(res) < EPS) or (abs(res + 1) < EPS):
+					res = np.dot(prod, seeds[1])
+					if (abs(res) < EPS) or (abs(res + 1) < EPS):
+						res = np.dot(prod, seeds[2])
+						if (abs(res) < EPS) or (abs(res + 1) < EPS):
+							verts[cache_size] = vecs[i]
+							cache_size += 1
 	return verts[:cache_size]
 
 filepath=sys.argv[1] #get input filepath
